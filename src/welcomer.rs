cd3f84115313b238c174fee7026d28f2615c3283
bachrc <8.bachrc@gmail.com>
2021-07-30 17:12:00 +0200

    Refacto en objet.

--- src/welcomer.rs
+++ src/welcomer.rs
@@ -3,11 +3,11 @@ use crate::person::Person;
 pub struct Welcomer;
 
 impl Welcomer {
-    pub fn compute_welcome_message() -> String {
+    pub fn compute_default_welcome_message(&self) -> String {
         String::from("Bonjour !")
     }
 
-    pub fn compute_personnalized_welcome_message(person_to_welcome: &Person) -> String {
+    pub fn compute_personnalized_welcome_message(&self, person_to_welcome: &Person) -> String {
         String::from(
             format!("Bonjour {} ! Vous êtes pimpant.e aujourd'hui !", person_to_welcome.compute_display_name())
         )
--- tests/welcoming_test.rs
+++ tests/welcoming_test.rs
@@ -3,16 +3,18 @@ use bienveillance_core::welcomer::Welcomer;
 
 #[test]
 fn should_welcome_anyone() {
-    let welcome_message: String = Welcomer::compute_welcome_message();
+    let welcomer = Welcomer;
+    let welcome_message: String = welcomer.compute_default_welcome_message();
 
-    assert_eq!(welcome_message, "Bonjour !")
+    assert_eq!(welcome_message, "Bonjour !");
 }
 
 #[test]
 fn should_welcome_me_with_my_name() {
     let person_to_welcome = Person::with_name("Hervé", "Chasuble");
 
-    let welcome_message: String = Welcomer::compute_personnalized_welcome_message(&person_to_welcome);
+    let welcomer = Welcomer;
+    let welcome_message: String = welcomer.compute_personnalized_welcome_message(&person_to_welcome);
 
     assert_eq!(welcome_message, "Bonjour Hervé Chasuble ! Vous êtes pimpant.e aujourd'hui !");
 }
